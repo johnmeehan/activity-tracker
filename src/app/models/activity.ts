ca062e63c35a5698ae601a74bf4f479719c9c4b0
John Meehan <john.meehan1@gmail.com>
2017-10-14 18:11:28 +0100

    use the Activity model to build activities

--- src/app/models/activity.ts
+++ src/app/models/activity.ts
@@ -1,17 +1,14 @@
 export class Activity {
-  id: number;
-  name: string;
-  date: Date;
-  comments?: string;
-  distance?: number;
-  gpxData: string;
 
-  constructor(id: number, name: string, date: Date, comments: string, distance: number, gpxData: string) {
-    this.id = id;
-    this.name = name;
-    this.date = new Date(date);
-    this.comments = comments;
-    this.distance = distance;
-    this.gpxData = gpxData;
-  }
+  constructor(
+    public id: number,
+    public name: string,
+    public date: Date,
+    public comments: string,
+    public distance: number,
+    public gpxData: string) {
+
+    }
+
+    // process the gpx file and get attributes like name, date, distance, heart rate, elevation from it.
 }
--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -1,44 +1,9 @@
 import { Activity } from '../models/activity';
 
 export const SAVED_ACTIVITES: Activity[] = [
-  {
-    'id': 1,
-    'name': 'Camino',
-    'date': new Date('2017/08/31'),
-    'comments': 'Sarria to Portomarin',
-    'distance': 23.54,
-    'gpxData': '../../assets/gpx/2017-08-31.gpx'
-  },
-  {
-    'id': 2,
-    'name': 'Camino',
-    'date': new Date('2017/09/01'),
-    'comments': 'Portomarin to Palas dl Rei',
-    'distance': 23.85,
-    'gpxData': '../../assets/gpx/2017-09-01.gpx'
-  },
-  {
-    'id': 3,
-    'name': 'Camino',
-    'date': new Date('2017/09/02'),
-    'comments': 'Palas dl Rei to Aruza',
-    'distance': 30.27,
-    'gpxData': '../../assets/gpx/2017-09-02.gpx'
-  },
-  {
-    'id': 4,
-    'name': 'Camino',
-    'date': new Date('2017/09/03'),
-    'comments': 'Aruza to Pedrouzo',
-    'distance': 21.19,
-    'gpxData': '../../assets/gpx/2017-09-03.gpx'
-  },
-  {
-    'id': 5,
-    'name': 'Camino',
-    'date': new Date('2017/09/04'),
-    'comments': 'Pedrouzo to Santiago de Compostela',
-    'distance': 21.04,
-    'gpxData': '../../assets/gpx/2017-09-04.gpx'
-  },
+  new Activity( 1, 'Camino', new Date('2017/08/31'), 'Sarria to Portomarin', 23.54, '../../assets/gpx/2017-08-31.gpx'),
+  new Activity( 2, 'Camino', new Date('2017/09/01'), 'Portomarin to Palas dl Rei', 23.85, '../../assets/gpx/2017-09-01.gpx'),
+  new Activity( 3, 'Camino', new Date('2017/09/02'), 'Palas dl Rei to Aruza', 30.27, '../../assets/gpx/2017-09-02.gpx'),
+  new Activity( 4, 'Camino', new Date('2017/09/03'), 'Aruza to Pedrouzo', 21.19, '../../assets/gpx/2017-09-03.gpx'),
+  new Activity( 5, 'Camino', new Date('2017/09/04'), 'Pedrouzo to Santiago de Compostela', 21.04, '../../assets/gpx/2017-09-04.gpx')
 ];
